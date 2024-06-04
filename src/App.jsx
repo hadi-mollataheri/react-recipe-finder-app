59bd04af4cc8b98fea1b994ea01136ed85a90a92
Hadi <mhadimollataheri@gmail.com>
2024-06-04 16:58:34 +0330

    feat(App.jsx & assets): Create navbar and default image
    Create elements for navbar and very basic styles
    
    Create a default image

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import icon from './assets/icon-360px.png';
 
 function App() {
-
   return (
-    <div>
-      <p className='bg-red-300'>Hi there</p>
-    </div>
-  )
+    <>
+      <header>
+        <div id='icon'>
+          <img src={icon} alt='burger' width={30} />
+          <p>Hadi&apos;s Recipe Finder</p>
+        </div>
+        <div id='searchBox'>
+          <input type='search' className=' border-2 border-black pl-1' />
+        </div>
+      </header>
+      <img
+        src={icon}
+        alt='burger'
+        width={200}
+        className='opacity-50'
+      />
+    </>
+  );
 }
 
-export default App
+export default App;
+
