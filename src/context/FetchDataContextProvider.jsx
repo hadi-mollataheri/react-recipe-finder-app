d3b3f4095f177e60164ad6b00a4d949569c52f51
Hadi <mhadimollataheri@gmail.com>
2024-06-08 11:05:19 +0330

    Fix a bug first use .then on fetchData and inside it update fetched data state in FetchDataContextProvider

--- src/context/FetchDataContextProvider.jsx
+++ src/context/FetchDataContextProvider.jsx
@@ -5,17 +5,17 @@ import { fetchData } from '../utilities/edamamAPI';
 export const FetchedDataContext = createContext();
 
 function FetchDataContextProvider({ children }) {
-  const [fetchedDataState, setFetchedDataState] = useState(null);
+  const [fetchedDataState, setFetchedDataState] = useState(null); // fetchedDataState.hits[0].recipe.'label' or '...'
   const [userInput, setUserInput] = useState('');
-
+  // console.log(fetchedDataState.hits[0].recipe.url);
   const handleInputChange = ({ target }) => {
     setUserInput(target.value);
   };
 
   const handleSearchClick = () => {
-    setFetchedDataState(() => {
+    fetchData(userInput).then((data) => {
       if (userInput) {
-        return fetchData(userInput);
+        setFetchedDataState(data);
       }
     });
   };
